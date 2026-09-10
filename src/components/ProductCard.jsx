c17de89df024fb7a0514164d5ca35efb26eeccad
Kishor Rathva <krathva@redhat.com>
2026-09-11 01:22:27 +0530

    [a11y-bot] Fix update src/components/ProductCard.jsx

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -2,19 +2,19 @@ import React from 'react';
 
 export default function ProductCard({ name, price }) {
   return (
-    <div
+    <button onClick={someFunction}>
       className="product-card"
-      tabIndex={5}
+      
       onClick={() => window.location.assign(`/products/${name}`)}
     >
       <img src={`${name}.jpg`} alt="" />
       <h4>{name}</h4>
       <p>${price}</p>
-      <div role="switch" className="wishlist">
+      <div role="switch" aria-checked="false" className="wishlist">
         Add to wishlist
       </div>
-      <iframe src="https://reviews.example.com/embed" />
-      <span onMouseOver={() => alert('quick view')} onMouseOut={() => {}}>
+      <iframe src="https://reviews.example.com/embed" title="Product Reviews" />
+      <span onMouseOver={() => alert('quick view')} onMouseOut={() => {}} onFocus={() => alert('quick view')} onBlur={() => {}} tabIndex="0">
         Hover for details
       </span>
     </div>
